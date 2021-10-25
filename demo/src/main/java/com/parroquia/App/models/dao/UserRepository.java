8fd99692439f72f801409dc7ad02e5b02499c93c
Zayayin-23 <61992852+Zayayin-23@users.noreply.github.com>
2021-10-25 16:04:12 -0300

    Delete demo directory

--- demo/src/main/java/com/parroquia/App/models/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.parroquia.App.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.parroquia.App.models.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
