8560bd87c69c5b1bd47a6b33867627c209810728
Zayayin-23 <davidcalbucoy23@gmail.com>
2021-11-28 23:07:45 -0300

    Corrección propierty

--- demo/src/main/resources/import.sql
+++ demo/src/main/resources/import.sql
@@ -8,3 +8,5 @@ INSERT INTO `parroquiadb`.`roles` (`id`, `descripcion`, `nombre`) VALUES ('2', '
 INSERT INTO `parroquiadb`.`users_roles` (`user_id`, `role_id`) VALUES ('1', '1');
 INSERT INTO `parroquiadb`.`users_roles` (`user_id`, `role_id`) VALUES ('1', '2');
 INSERT INTO `parroquiadb`.`users_roles` (`user_id`, `role_id`) VALUES ('2', '1');
+INSERT INTO `parroquiadb`.`users_roles` (`user_id`, `role_id`) VALUES ('3', '2');
+
